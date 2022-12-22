6bc194c10ce60afbb7abe1c01c2eccc09ba0eb0f
JimHaff <james.haff@generalassemb.ly>
2022-12-22 10:42:04 -0600

    added detail page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Cat(models.Model):
+	# we're defining the columns and constraints on the rows for each column
+	name = models.CharField(max_length=100)
+	breed = models.CharField(max_length=100)
+	description = models.TextField(max_length=250)
+	age = models.IntegerField()
--- main_app/views.py
+++ main_app/views.py
@@ -2,29 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
-##############################################
-# THIS IS SIMULATING A MODEL, JUST FOR TODAY, 
-# SO WE HAVE DATA TO INJECT INTO OUR TEMPLATES
-class Cat:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-# This the array, we are injecting into the template
-cats = [
-  Cat('Lolo', 'tabby', 'foul little demon', 3),
-  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
-
-
-# After this lesson this code will not be used
-# because we'll use a REAL MODEL that can talk to 
-# the DB
-##############################################
+from .models import Cat
 
 def home(request):
 	return render(request, 'home.html')
@@ -37,4 +15,18 @@ def about(request):
 def cats_index(request):
 	# the key on the dictionary is the variable name
 	# in the template (index.html)
-	return render(request, 'cats/index.html', {'cats': cats})
+  #Cat is our model, that can talk to sql and retrieve all the rows in our table
+  cats = Cat.objects.all()
+  return render(request, 'cats/index.html', {'cats': cats})
+
+
+# cat_id is coming from the urls.py, params 
+#path('cats/<int:cat_id>/' <--------
+# they must be the same, Django convention!
+def cats_detail(request, cat_id):
+  # use our model Cat (Capital cat) to retrieve whatever row
+  # from our db the cat_id matches
+  cat = Cat.objects.get(id=cat_id)
+  # cats/detail.html <- refers to a template
+  # render responds to the client
+  return render(request, 'cats/detail.html', {'cat': cat})
